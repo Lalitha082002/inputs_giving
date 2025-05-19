22e61000e6d2ac8163ea32a38756b985af556493
Lalitha082002 <rlalithadhanunjaya@gmail.com>
2025-05-19 12:07:30 +0530

    Create main.py

---
+++ .github/workflows/main.py
@@ -0,0 +1,9 @@
+def check_age(a):
+  return int(age) > 18
+
+if __name__ == "__main__":
+  age = sys.argv[1]
+  result = check_age(age)
+  with open("result.txt", "w") as f:
+    f.write(str(result))
+    
